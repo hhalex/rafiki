e7f04a0f7bd81e7ced63d168207452d926b0874f
Alexandre Careil <alex.careil.fr@gmail.com>
2020-12-10 16:24:56 +0100

    ✨ Add dumb Login component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,44 +1,5 @@
-import { Grid, makeStyles, Paper } from "@material-ui/core";
 import React from "react";
-
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      '& > *': {
-        fontSize: "24px",
-        padding: theme.spacing(2),
-        margin: theme.spacing(3),
-        width: theme.spacing(32),
-        height: theme.spacing(5),
-      },
-    },
-  }));
-  
-const SimplePaper = ({children}: any) => {
-    const classes = useStyles();
-  
-    return (
-      <div className={classes.root}>
-        <Paper>{children}</Paper>
-      </div>
-    );
-  }
+import { Login } from "./pages/Login";
 
 export const App = () =>
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <SimplePaper>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >Welcome on Rafiki</Grid>
-      </SimplePaper>
-    </Grid>
+    <Login />
