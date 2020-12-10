aaf9d28c9dd10e531e7aa703509f667d7d4eed5f
Alexandre Careil <alex.careil.fr@gmail.com>
2020-12-10 16:09:48 +0100

    Extract main component App from root

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,51 +1,11 @@
-import { Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 import './index.css';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      fontSize: "24px",
-      padding: theme.spacing(2),
-      margin: theme.spacing(3),
-      width: theme.spacing(32),
-      height: theme.spacing(5),
-    },
-  },
-}));
-
-const SimplePaper = ({children}: any) => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Paper>{children}</Paper>
-    </div>
-  );
-}
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <SimplePaper>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >Welcome on Rafiki</Grid>
-      </SimplePaper>
-    </Grid>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
