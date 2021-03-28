11b84c4ed946e666ccec27e945cc0edec10b899c
Alexandre Careil <alex.careil.fr@gmail.com>
2021-03-29 00:33:35 +0200

    Add CompanyContract model

---
+++ src/main/scala/com/lion/rafiki/domain/CompanyContract.scala
@@ -0,0 +1,73 @@
+package com.lion.rafiki.domain
+
+import cats.Monad
+import cats.data.{EitherT, OptionT}
+import cats.implicits.toFunctorOps
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import shapeless.tag
+import shapeless.tag.@@
+import tsec.passwordhashers.PasswordHasher
+import tsec.passwordhashers.jca.BCrypt
+
+final case class CompanyContract[Company](company: Company, kind: CompanyContract.Kind) {
+  def withId(id: CompanyContract.Id) = WithId(id, this)
+}
+
+object CompanyContract {
+  sealed trait Kind extends Product with Serializable
+  object Kind {
+    case object OneShot extends Kind
+    case object Unlimited extends Kind
+  }
+
+  type Id = Long @@ CompanyContract[_]
+  val tagSerial = tag[CompanyContract[_]](_: Long)
+
+  implicit val companyContractIdDecoder: Decoder[Id] = Decoder[Long].map(CompanyContract.tagSerial)
+  implicit val companyContractIdEncoder: Encoder[Id] = Encoder[Long].contramap(_.asInstanceOf[Long])
+  implicit val companyContractKindDecoder: Decoder[Kind] = Decoder[String].emap(_.toLowerCase match {
+    case "unlimited" => Right(Kind.Unlimited)
+    case "oneshot" => Right(Kind.OneShot)
+    case other => Left(s"'$other' is not a member value of CompanyContract.Kind")
+  })
+  implicit val companyContractKindEncoder: Encoder[Kind] = Encoder[String].contramap(_.toString)
+  implicit val companyContractCreateDecoder: Decoder[CreateRecord] = deriveDecoder
+  implicit val companyContractCreateEncoder: Encoder[CreateRecord] = deriveEncoder
+  implicit val companyContractWithIdDecoder: Decoder[Record] = WithId.decoder
+  implicit val companyContractWithIdEncoder: Encoder[Record] = WithId.encoder
+
+  type CreateRecord = CompanyContract[Company.Id]
+  type Record = WithId[Id, CreateRecord]
+
+  trait Repo[F[_]] {
+    def create(c: CreateRecord): F[Record]
+    def update(c: Record): OptionT[F, Record]
+    def get(id: Id): OptionT[F, Record]
+    def delete(id: Id): OptionT[F, Record]
+    def list(pageSize: Int, offset: Int): F[List[Record]]
+  }
+
+  class Service[F[_]: Monad](companyContractRepo: Repo[F], companyValidation: Company.Validation[F])(implicit P: PasswordHasher[F, BCrypt]) {
+    def create(companyContract: CreateRecord): EitherT[F, ValidationError, Record] =
+      for {
+        _ <- companyValidation.exists(companyContract.company)
+        saved <- EitherT.liftF(companyContractRepo.create(companyContract))
+      } yield saved
+
+    def update(companyContract: Record): EitherT[F, ValidationError, Record] =
+      EitherT.fromOptionF(
+        companyContractRepo.update(companyContract).value,
+        ValidationError.CompanyContractNotFound: ValidationError
+      )
+
+    def get(id: Id): EitherT[F, ValidationError, Record] =
+      EitherT.fromOptionF(companyContractRepo.get(id).value, ValidationError.CompanyContractNotFound: ValidationError)
+
+    def delete(id: Id): F[Unit] = companyContractRepo.delete(id).value.as(())
+
+    def list(pageSize: Int, offset: Int): F[List[Record]] = companyContractRepo.list(pageSize, offset)
+  }
+}
+
+
--- src/main/scala/com/lion/rafiki/domain/ValidationError.scala
+++ src/main/scala/com/lion/rafiki/domain/ValidationError.scala
@@ -5,6 +5,7 @@ sealed trait ValidationError extends Product with Serializable
 object ValidationError {
   case object UserNotFound extends ValidationError
   case object CompanyNotFound extends ValidationError
+  case object CompanyContractNotFound extends ValidationError
   case class UserAlreadyExists(data: User.Record) extends ValidationError
   case class CompanyAlreadyExists(data: Company.Record) extends ValidationError
 }
