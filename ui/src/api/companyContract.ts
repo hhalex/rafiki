4840c4e30f4dbf65e52734c4211cf5dffbd9ba93
Alexandre Careil <alex.careil.fr@gmail.com>
2021-04-01 16:47:00 +0200

    ✨️Add comanyContract front api

---
+++ ui/src/api/companyContract.ts
@@ -0,0 +1,31 @@
+import { AuthenticatedFetch } from "../atoms/Auth";
+import { WithId } from "./WithId";
+
+export type CompanyContract<Company> = {
+    company: Company,
+    kind: CompanyContract.Kind
+};
+
+export module CompanyContract {
+    export const enum Kind {
+        Unlimited = "unlimited",
+        OneShot = "oneshot"
+    }
+
+    export type Create = CompanyContract<string>;
+    export type Update = WithId<Create>;
+    export type Full = WithId<CompanyContract<string>>
+
+    export const createApi = (authFetch: AuthenticatedFetch) => ({
+        create: (companyContract: Create) =>
+            authFetch.post(`/api/company/${companyContract.company}/contract`, JSON.stringify(companyContract)).then<Full>(b => b.json()),
+        update: (companyContract: Update) =>
+            authFetch.put(`/api/company/${companyContract.company}/contract/${companyContract.id}`, JSON.stringify(companyContract)).then<Full>(b => b.json()),
+        delete: ({ company, id }: Update) =>
+            authFetch.delete(`/api/company/${company}/contract/${id}`),
+        listByCompany: (companyId: string, pageSize?: number, offset?: number) => 
+            authFetch.get(`/api/company/${companyId}/contract`).then<Full[]>(b => b.json()),
+        list: (companyId: string, pageSize?: number, offset?: number) => 
+            authFetch.get(`/api/company/${companyId}/contract`).then<Full[]>(b => b.json()),
+    });
+}
