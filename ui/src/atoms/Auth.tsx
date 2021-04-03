6d278f67aaec99b53e96a47974ee1ad24932c5d5
Alexandre Careil <alex.careil.fr@gmail.com>
2021-04-03 16:02:42 +0200

    ✨️Persist authentication in local storage

--- ui/src/atoms/Auth.tsx
+++ ui/src/atoms/Auth.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent } from "@material-ui/core";
 import * as React from "react";
+import { useEffect } from "react";
 import { atom, SetterOrUpdater, useRecoilState } from "recoil";
 import { createLoginApi } from "../api/login";
 import { Login } from "../pages/Login";
@@ -24,6 +25,19 @@ export enum Role {
     Employee = "Employee"
 };
 
+export namespace Role {
+    export const fromStr = (str: string | null): Role | null => {
+        switch (str) {
+            case Role.Admin:
+            case Role.Company:
+            case Role.Employee:
+                return str as Role;
+            default:
+                return null;
+        }
+    }
+}
+
 export type AuthenticatedFetch = {
     get: SimplifiedFetch,
     post: SimplifiedFetch,
@@ -37,18 +51,54 @@ export const authenticatedFetchAtom = atom<AuthenticatedFetch | undefined>({
     default: undefined
 });
 
+type TokenAndRole = {
+    token: string,
+    role: Role
+};
+
+namespace TokenAndRole {
+    const tokenKey = "RELIONS_AUTH_TOKEN";
+    const roleKey = "RELIONS_AUTH_ROLE";
+    /**
+     * Try extract authentication token and role from local storage
+     * @returns Properly formatted authentication token & role, or null in case of error 
+     */
+    export const get = (): TokenAndRole | null => {
+        const token = localStorage.getItem(tokenKey);
+        const role = Role.fromStr(localStorage.getItem(roleKey));
+        return token && role ? { token, role } : null;
+    };
+    /**
+     * Store authentication token and role into the local storage
+     */
+    export const persist = ({token, role}: TokenAndRole) => {
+        localStorage.setItem(tokenKey, token);
+        localStorage.setItem(roleKey, role);
+    };
+    /**
+     * Remove persisted authentication token & role from local storage
+     */
+    export const clean = () => {
+        localStorage.removeItem(tokenKey);
+        localStorage.removeItem(roleKey);
+    };
+}
+
 const AuthHeader = "Authorization";
 
-export const updateAuthenticatedFetchWithResponse = async (response: Response, setter: SetterOrUpdater<AuthenticatedFetch | undefined>) => {
+export const updateAuthenticatedFetchWithLoginResponse = async (response: Response, setter: SetterOrUpdater<AuthenticatedFetch | undefined>) => {
     const updatedBearerToken = response.status === 401
-        ? undefined
-        : response.headers.get(AuthHeader) ?? undefined;
-    const role = await response.json() as Role;
-    setter(_ => 
-        updatedBearerToken 
-            ? createAuthenticatedFetch(updatedBearerToken, role, setter)
-            : undefined
-    );
+        ? null
+        : response.headers.get(AuthHeader);
+    const role = Role.fromStr(await response.json());
+    setter(_ => {
+        if (updatedBearerToken && role) {
+            TokenAndRole.persist({ token: updatedBearerToken, role });
+            return createAuthenticatedFetch(updatedBearerToken, role, setter)
+        } else {
+            TokenAndRole.clean()
+        }
+    });
     return response;
 }
 
@@ -57,7 +107,10 @@ export const createAuthenticatedFetch = (bearerToken: string, role: Role, setter
         (url: string, body?: string | number) =>
             fetch(url, { headers: [[AuthHeader, bearerToken]], method, body: body as string })
                 .then(response => {
-                    if(response.status === 401) setter(undefined);
+                    if(response.status === 401) {
+                        TokenAndRole.clean();
+                        setter(undefined);
+                    }
                     return response;
                 });
 
@@ -73,8 +126,21 @@ export const createAuthenticatedFetch = (bearerToken: string, role: Role, setter
 export const AuthProvider = ({ children }: any) => {
     const [authFetch, setAuthFetch] = useRecoilState(authenticatedFetchAtom);
 
+    // If not authenticated, try using persisted authentication
+    useEffect(() => {
+        console.log("use effect try get persisted auth")
+        if (!authFetch) {
+            const auth = TokenAndRole.get();
+            if (auth)
+                setAuthFetch(
+                    createAuthenticatedFetch(auth.token, auth.role, setAuthFetch)
+                );
+        }
+    }, [authFetch]);
+
+
     const LoginModal = () => {
-        const loginApi = createLoginApi(r => updateAuthenticatedFetchWithResponse(r, setAuthFetch));
+        const loginApi = createLoginApi(r => updateAuthenticatedFetchWithLoginResponse(r, setAuthFetch));
         return <Dialog open={true} aria-labelledby="form-dialog-title">
             <DialogContent>
                 <Login api={loginApi} />
