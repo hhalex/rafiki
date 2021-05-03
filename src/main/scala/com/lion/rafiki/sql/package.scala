9b4e0e185a5e4208741390498f198a532e123a43
Alexandre Careil <alex.careil.fr@gmail.com>
2021-05-03 18:10:44 +0200

    🐛 Add missing package file

---
+++ src/main/scala/com/lion/rafiki/sql/package.scala
@@ -0,0 +1,13 @@
+package com.lion.rafiki
+
+import com.lion.rafiki.domain.TaggedId
+import com.lion.rafiki.auth.PasswordHasher
+import cats.effect.MonadCancel
+
+import doobie.util.meta.Meta
+
+package object sql {
+    type TaglessMonadCancel[F[_]] = MonadCancel[F, Throwable]
+    def createMetaId[Id](t: TaggedId): Meta[t.Id] = Meta[Long].imap(t.tag)(t.unTag)
+    def createMetaPasswd(): Meta[PasswordHasher.Password]= Meta[String].imap(PasswordHasher.tag)(_.asInstanceOf[String])
+}
